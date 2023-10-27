9fcd9c2d98d7932e1b41aac0d043789a86ed6cc7
yukesh73 <149184822+yukesh73@users.noreply.github.com>
2023-10-27 21:14:55 +0530

    Add files via upload

---
+++ Unit-3 Challenges/linear search product.py
@@ -0,0 +1,9 @@
+def linear_search_product(product_list, target_product):
+indices = []
+
+# Loop through the list of products
+for i in range(len(product_list)):
+    if product_list[i] == target_product:
+        indices.append(i)
+
+return indices
